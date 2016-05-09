d6af7b86944eef254b1d9e9dde25b219a51cb8a9
Istvan Steiner <istvan.steiner@ericsson.com>
2016-05-09 09:43:13 +0200

    First working version. Some checks are not implemented yet.

--- Backup2Iomega.sh
+++ Backup2Iomega.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+short_name=${0##*/}
 actual_subdirectory="$1"
 actual_date=$(date +%Y%m%d)
 hdd_tempdir_for_oldbackup="/home/ethsri/Temp4BackupIomega"
 actual_directory="${hdd_tempdir_for_oldbackup}/${actual_subdirectory}"
 backup_path="/media/ethsri/Iomega HDD/Backup"
+
+# Check command line parameters
+if [ $# -ne 1 ]
+then
+    echo "ERROR! Wrong number of parameters."
+    echo "Usage: ${short_name} <directory name under Backup on Iomega>"
+    exit 1
+fi
 
 # check that backup tool is mounted
 
@@ -12,10 +21,10 @@ backup_path="/media/ethsri/Iomega HDD/Backup"
 # Check that we have enough space on backup tool
 
 
-# Do the backup
-cp -rp /home/ethsri/Documents/Private/ "${backup_path}/Private_${actual_date}"
+echo "Making the backup..."
 
-exit 2
+cp -rp /home/ethsri/Documents/Private/ "${backup_path}/Private_${actual_date}"
+cp -rp /home/ethsri/Documents/Common/ "${backup_path}/Common_${actual_date}"
 
 # Check if the old backup exists
 
@@ -23,21 +32,23 @@ exit 2
 # Check if we have enough space on the own HDD
 
 
-# Copy the old backup to the own HDD
-cp -rp ${backup_path}/${actual_subdirectory} ${hdd_tempdir_for_oldbackup}/
+echo "Copying the old backup to the own HDD..."
+cp -rp "${backup_path}/${actual_subdirectory}" ${hdd_tempdir_for_oldbackup}/
 
+echo "Getting rid of single quotes in filenames..."
 find ${actual_directory} -type f -name "*'*" -exec rename "s/\'/-/" "{}" + ;
 
+echo "Getting rid of MSDOS line endings in text files..."
 IFS=$'\n'
 for myfile in $(find ${actual_directory} -iname \*.txt -o -iname \*.htm\* -o -iname \*.css); do if /usr/bin/file "${myfile}" | grep -q "with CRLF line terminators"; then sed -i 's/\r$//' "${myfile}" ; fi; done
 
+echo "Searching for duplicate files..."
 cd /home/ethsri/Documents/Common/Scripts/DupliSeek
 ./dupliSeek.py -r /home/ethsri/Documents/ -s ${actual_directory} > ${hdd_tempdir_for_oldbackup}/duplicate_files.txt
 
+echo "Removing found duplicate files in the HDD..."
 grep Temp4BackupIomega ${hdd_tempdir_for_oldbackup}/duplicate_files.txt | xargs -I{} rm {}
+
+echo "Deleting zero size files and empty directories..."
 find ${actual_directory} -type f -size 0 -delete
 find ${actual_directory} -type d -empty -delete
-
-
-
-cp -rp ${hdd_tempdir_for_oldbackup}/ /tmp/Private_${actual_date}
