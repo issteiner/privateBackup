f0578a14aa3d30ccbb0aec2d3a3676627e261779
Istvan Steiner <istvan.steiner@ericsson.com>
2016-11-14 07:39:34 +0100

    Add remove old dirs and some checks.

--- Backup2Iomega.sh
+++ Backup2Iomega.sh
@@ -1,39 +1,70 @@
 #!/bin/bash
+# This script makes the backup of the current Private and Common directory to IOmega, then copies Private back.
+# After the successful copy, removes the old - the date is the command line parameter - directories from IOmega.
 
 short_name=${0##*/}
-actual_subdirectory="$1"
+old_backup_date="$1"
+private_dir_on_iomega=Private_${old_backup_date}
 actual_date=$(date +%Y%m%d)
 hdd_tempdir_for_oldbackup="/home/ethsri/Temp4BackupIomega"
-actual_directory="${hdd_tempdir_for_oldbackup}/${actual_subdirectory}"
-backup_path="/media/ethsri/Iomega HDD/Backup"
+actual_directory="${hdd_tempdir_for_oldbackup}/${private_dir_on_iomega}"
+BACKUP_PATH="/media/ethsri/Iomega HDD/Backup"
+DIRS_TO_BACKUP="Common Private"
+
+function exit_if_error {
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR occurred during $2. Exiting..."
+        exit 1
+    fi
+}
 
 # Check command line parameters
 if [ $# -ne 1 ]
 then
     echo "ERROR! Wrong number of parameters."
-    echo "Usage: ${short_name} <directory name under Backup on Iomega>"
+    echo "Usage: ${short_name} <date of backup under Backup on Iomega>"
     exit 1
 fi
 
-# check that backup tool is mounted
+# Check that backup tool is mounted
+if [ ! -d "${BACKUP_PATH}" ]
+then
+    echo "ERROR! Iomega is not connected/mounted."
+    exit 1
+fi
 
+# Check if the old backup exists
+if [ ! -d "${BACKUP_PATH}/${private_dir_on_iomega}" ]
+then
+    echo "ERROR! File ${private_dir_on_iomega} does not exist in ${BACKUP_PATH}."
+    exit 1
+fi
 
 # Check that we have enough space on backup tool
 
-
 echo "Making the backup..."
-
-cp -rp /home/ethsri/Documents/Private/ "${backup_path}/Private_${actual_date}"
-cp -rp /home/ethsri/Documents/Common/ "${backup_path}/Common_${actual_date}"
-
-# Check if the old backup exists
-
+for dir in ${DIRS_TO_BACKUP}
+do
+    cp -rp /home/ethsri/Documents/${dir}/ "${BACKUP_PATH}/${dir}_${actual_date}"
+    exit_if_error $? "copying ${dir} to ${BACKUP_PATH}"
+done
 
 # Check if we have enough space on the own HDD
 
 
 echo "Copying the old backup to the own HDD..."
-cp -rp "${backup_path}/${actual_subdirectory}" ${hdd_tempdir_for_oldbackup}/
+cp -rp "${BACKUP_PATH}/${private_dir_on_iomega}" ${hdd_tempdir_for_oldbackup}/
+exit_if_error $? "copying ${private_dir_on_iomega} to ${hdd_tempdir_for_oldbackup}"
+
+echo "Removing old directories on the backup device..."
+for dir in "${DIRS_TO_BACKUP}"
+do
+    if [ -d "${BACKUP_PATH}/${dir}_${old_backup_date}" ]
+    then
+        rm -rf "${BACKUP_PATH}/${dir}_${old_backup_date}"
+    fi
+done
 
 echo "Getting rid of single quotes in filenames..."
 find ${actual_directory} -type f -name "*'*" -exec rename "s/\'/-/" "{}" + ;
@@ -52,3 +83,4 @@ grep Temp4BackupIomega ${hdd_tempdir_for_oldbackup}/duplicate_files.txt | xargs 
 echo "Deleting zero size files and empty directories..."
 find ${actual_directory} -type f -size 0 -delete
 find ${actual_directory} -type d -empty -delete
+
